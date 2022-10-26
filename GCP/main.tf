01fcfe58a2c6cbdc91cfba937239e01edf950a3e
Claudio Cruz <zero@serversup.com>
2022-10-26 14:01:20 -0400

    Updates and GCP

--- GCP/main.tf
+++ GCP/main.tf
@@ -18,12 +18,36 @@ provider "google" {
 
 }
 
+provider "tls" {
+  // no config needed
+}
+
+resource "tls_private_key" "linux_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "ssh_private_key_pem" {
+  content         = tls_private_key.linux_key.private_key_pem
+  filename        = "linuxkey.pem"
+  file_permission = "400"
+}
+
+# Virtual Private Network
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
 module "cribl-leader" {
-  source = ".//modules/leader"
+  source   = ".//modules/leader"
+  network  = google_compute_network.vpc_network.name
+  ssh-key  = tls_private_key.linux_key.public_key_openssh
+  username = var.username
+
 }
 
 module "cribl-workers" {
-  source = ".//modules/workers"
+  source        = ".//modules/workers"
   workers_count = var.workers_count
 }
 
