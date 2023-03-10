baeed3d85bdca66713f862877f49beffd8a71f95
cblcruz <112976647+cblcruz@users.noreply.github.com>
2023-03-09 21:41:56 -0300

    Delete AWS directory

--- AWS/movefiles.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-sudo su root <<EOF
-sudo mv /tmp/authorized_keys /home/povadmin/.ssh/authorized_keys
-sudo chown -R povadmin:povadmin /home/povadmin/.ssh/authorized_keys
-EOF
--- AWS/start/script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-jQuery.noConflict();
-jQuery(document).ready(function ($) {
-
-    // usage: 2
-    $('#myForm').formToJson('.result-json-output');
-
-});
